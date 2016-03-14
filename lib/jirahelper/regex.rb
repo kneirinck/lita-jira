9f78a4833683e141321de926c56cb4ff88223bb4
Klaas Neirinck <klaas.neirinck@pegus-apps.com>
2016-03-14 22:13:49 +0100

    allow multiline description and summary when creating an issue

--- lib/jirahelper/regex.rb
+++ lib/jirahelper/regex.rb
@@ -5,8 +5,8 @@ module JiraHelper
     QUOTE_PATTERN = /[\"“”]/
     TYPE_PATTERN = /(?<type>\w+)/
     COMMENT_PATTERN = /#{QUOTE_PATTERN}(?<comment>.+)#{QUOTE_PATTERN}/
-    DESCRIPTION_PATTERN = /#{QUOTE_PATTERN}(?<description>.+?)#{QUOTE_PATTERN}/
-    SUMMARY_PATTERN = /#{QUOTE_PATTERN}(?<summary>.+?)#{QUOTE_PATTERN}/
+    DESCRIPTION_PATTERN = /#{QUOTE_PATTERN}(?<description>.+?)#{QUOTE_PATTERN}/m
+    SUMMARY_PATTERN = /#{QUOTE_PATTERN}(?<summary>.+?)#{QUOTE_PATTERN}/m
     PROJECT_PATTERN = /(?<project>[a-zA-Z0-9]{1,10})/
     ISSUE_PATTERN   = /(?<issue>#{PROJECT_PATTERN}-[0-9]{1,5}+)/
     EMAIL_PATTERN   = /(?<email>[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+)/i
